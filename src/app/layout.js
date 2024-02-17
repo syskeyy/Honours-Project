16e29770162a403676aeb7624847c9abb7186a20
syskeyy <dgulijev@gmail.com>
2024-02-17 15:42:01 +0000

    bug fix with provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,21 @@
 import { Inter } from 'next/font/google'
 import './ui/globals.css'
-import Provider from '../context/provider'
+import Provider from '../context/Provider'
 
 const inter = Inter({ subsets: ['latin'] })
-
 export const metadata = {
   title: 'ChainSafe | Bicycle Maintenance Tracker',
   description: 'Bicycle Maintenance Tracker With Dashboard',
+  icons: {
+    icon: 'logo.ico',
+  }
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Provider>
-      <body className={inter.className}>{children}</body>
+        <body className={inter.className}>{children}</body>
       </Provider>
     </html>
   )
